eb38ce878bb7beb240fdfe1260407531242f93e5
Mohamed Gomaa <mgom3a@gmail.com>
2022-06-16 09:56:03 +0300

    add some files & folders

---
+++ sites/default/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'sites/default/files/.ht.sqlite',
+      'driver' => 'sqlite',
+      'prefix' => '',
+    ),
+  ),
+);
+$drupal_hash_salt = 'q2RaYSZLqwX1WwaL8lBxzsb7223kIWZNDFJKYEeYci8';
